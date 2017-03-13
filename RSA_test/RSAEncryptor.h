bf9e6f488b8eeccc4543a370b8e54c5c36cb4f91
“zhouzhiyonglog git config  --global user.email “zhouzhiyong_work@163.com <zhouzhiyong_work@163.com git config  --global user.email zhouzhiyong_work@163.com>
2017-03-13 12:45:53 +0800

    zzz

---
+++ RSA_test/RSAEncryptor.h
@@ -0,0 +1,58 @@
+//
+//  RSAEncryptor.h
+//  RSA_test
+//
+//  Created by 周智勇 on 17/3/13.
+//  Copyright © 2017年 周智勇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSAEncryptor : NSObject
+
+/**
+ *  加密方法
+ *
+ *  @param str   需要加密的字符串
+ *  @param path  '.der'格式的公钥文件路径
+ */
++ (NSString *)encryptString:(NSString *)str publicKeyWithContentsOfFile:(NSString *)path;
+
+/**
+ *  解密方法
+ *
+ *  @param str       需要解密的字符串
+ *  @param path      '.p12'格式的私钥文件路径
+ *  @param password  私钥文件密码
+ */
++ (NSString *)decryptString:(NSString *)str privateKeyWithContentsOfFile:(NSString *)path password:(NSString *)password;
+
+/**
+ *  加密方法
+ *
+ *  @param str    需要加密的字符串
+ *  @param pubKey 公钥字符串
+ */
++ (NSString *)encryptString:(NSString *)str publicKey:(NSString *)pubKey;
+
+/**
+ *  解密方法
+ *
+ *  @param str     需要解密的字符串
+ *  @param privKey 私钥字符串
+ */
++ (NSString *)decryptString:(NSString *)str privateKey:(NSString *)privKey;
+
+
+//------------------新增方法
+
+/**
+ 加密方法。（原创之后前面的方法，为了解决直接对字典进行加密，所以就加了这个，其他的视情况而定）
+
+ @param dictionary 需要加密的字典
+ @param path       '.der'格式的公钥文件路径
+
+ @return 加密之后的值
+ */
++ (NSString *)encryptDictionary:(NSDictionary *)dictionary publicKeyWithContentsOfFile:(NSString *)path;
+@end
--- RSA_test/ViewController.m
+++ RSA_test/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "RSAEncryptor.h"
 @interface ViewController ()
 
 @end
@@ -16,8 +16,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    /*
+    字符串加密
+    //原始数据
+    NSString *originalString = @"这是一段将要使用'.der'文件加密的字符串!";
+//    使用.der和.p12中的公钥私钥加密解密
+    NSString *public_key_path = [[NSBundle mainBundle] pathForResource:@"public_key.der" ofType:nil];
+    NSString *private_key_path = [[NSBundle mainBundle] pathForResource:@"private_key.p12" ofType:nil];
+    
+    NSString *encryptStr = [RSAEncryptor encryptString:originalString publicKeyWithContentsOfFile:public_key_path];
+    NSLog(@"加密前:%@", originalString);
+    NSLog(@"加密后:%@", encryptStr);
+    NSLog(@"解密后:%@", [RSAEncryptor decryptString:encryptStr privateKeyWithContentsOfFile:private_key_path password:@"123456"]);
+    */
+    
+    //字典加密
+    NSMutableDictionary * allParams = [NSMutableDictionary dictionary];
+    [allParams setObject:@"useridididdiid" forKey:@"userid"];
+    [allParams setObject:@"tokentoken" forKey:@"token"];
+    [allParams setObject:@"versionversion" forKey:@"version"];
+    
+    NSString *public_key_path = [[NSBundle mainBundle] pathForResource:@"public_key.der" ofType:nil];
+    NSString *private_key_path = [[NSBundle mainBundle] pathForResource:@"private_key.p12" ofType:nil];
+    
+    NSString *encryptStr = [RSAEncryptor encryptDictionary:allParams publicKeyWithContentsOfFile:public_key_path];
+    NSLog(@"加密前:%@", allParams);
+    NSLog(@"加密后:%@", encryptStr);
+    NSLog(@"解密后:%@", [RSAEncryptor decryptString:encryptStr privateKeyWithContentsOfFile:private_key_path password:@"123456"]);
 }
+
+
 
 
 - (void)didReceiveMemoryWarning {
